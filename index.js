aeada1d01f3bdfccecc8175bf678f06e3d720542
minyoungshin <mins6649@gmail.com>
2023-02-03 10:22:31 -0500

    for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    let newMessage = [];
+    for(let i=0;i<array.length;i++){
+        newMessage.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newMessage;
+}
+function countDown(number){
+    let i=0;
+    while (i <= number){
+        console.log(number-i);
+        i++;
+    }
+
+}
